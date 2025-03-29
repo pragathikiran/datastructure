55089634cbfb33344257d62f8dc8f7f6d66fea98
pragathikiran <pragathi20.kiran@gmail.com>
2025-03-29 20:13:46 +0530

    Add files via upload

---
+++ 4list.py
@@ -0,0 +1,18 @@
+list = ['mango','apple','banana','kiwi']
+print("list length",len(list))
+
+print("first element",list[0])
+
+print("last element",list[-1])
+
+list.append('papaya')
+print("update list",list)
+
+list.sort()
+print("sorted list",list)
+
+list.pop(1)
+print("updated list",list)
+
+list.reverse()
+print("reverese list",list)
