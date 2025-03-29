d9997dde3dd06e55cd9f6f743bccc7275b8f8165
pragathikiran <pragathi20.kiran@gmail.com>
2025-03-29 20:26:27 +0530

    Add files via upload

---
+++ 5dictionary.py
@@ -0,0 +1,8 @@
+dict = {}
+dict = {1: 'apple',2: 'banana' }
+dict = {'namae':'jack','age':26}
+dict =  ['age']=27
+print(dict)
+dict['address']='downtown'
+print(dict)
+print("adress:",dict.get(address))
